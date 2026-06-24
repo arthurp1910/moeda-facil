6454619c5b7cf0f9ef6f775c452013a2cb3e9ea4
arthurp1910 <arthurpaulo983751@gmail.com>
2026-06-24 17:10:31 -0300

    colocando responsividade em alguns containers

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -6,20 +6,21 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <footer className="footer">
-      <div className="footerCentral">
-        © 2026 Moeda Fácil 
-      </div>
+      <div className="footerCentral">© 2026 Moeda Fácil</div>
 
-      <div className="footerDesenvolvido">
-        Desenvolvido por Arthur Lemes
-      </div>
+      <div className="footerDesenvolvido">Desenvolvido por Arthur Lemes</div>
 
       <div className="footerLinks">
-        <img src={githubLogo} alt="GitHub" width="30" height="30" />
-        <img src={emailLogo} alt="Email" width="30" height="30" />
-        <img src={linkedinLogo} alt="LinkedIn" width="30" height="30" />
+        <a href="https://github.com/arthurp1910">
+          <img src={githubLogo} alt="GitHub" width="30" height="30" />
+        </a>
+        <a href="mailto:lemesarthurr@gmail.com?subject=Assunto&body=Mensagem">
+          <img src={emailLogo} alt="Email" width="30" height="30" />
+        </a>
+        <a href="https://www.linkedin.com/in/arthur-lemes-58b69926a">
+          <img src={linkedinLogo} alt="LinkedIn" width="30" height="30" />
+        </a>
       </div>
-
     </footer>
   );
-}                             
+}
