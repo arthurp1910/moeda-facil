4ec6bc99ef69922aec62772ce6115745464c49a9
Arthur Lemes <arthurpaulo983751@email.com>
2026-06-26 20:32:15 -0300

    feat: adicionar funcionalidade de listagem de moedas e melhorar tratamento de erros na conversão

--- src/pages/CurrencyConverter.tsx
+++ src/pages/CurrencyConverter.tsx
@@ -1,6 +1,6 @@
 import "./CurrencyConverter.css";
-import { converterMoeda } from "../services/api";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { converterMoeda, listarMoedas } from "../services/api";
 
 export default function CurrencyConverter() {
   const [valor, setValor] = useState("");
@@ -8,20 +8,37 @@ export default function CurrencyConverter() {
   const [moedaDestino, setMoedaDestino] = useState("BRL");
   const [resultado, setResultado] = useState<number | null>(null);
   const [taxa, setTaxa] = useState<number | null>(null);
+  const [erro, setErro] = useState<string | null>(null);
+  const [todasAsMoedas, setTodasAsMoedas] = useState([]);
+  const [busca, setBusca] = useState("");
+  const [moedasFiltradas, setMoedasFiltradas] = useState([]);
+  const [mostrarLista, setMostrarLista] = useState(false);
 
-  async function handleConverter(event: React.FormEvent<HTMLFormElement>) {
+  useEffect(() => {
+    listarMoedas().then((dadosLista) => {
+      setTodasAsMoedas(dadosLista.supported_codes);
+    });
+  }, []);
+  async function handleConverter(event: React.SyntheticEvent<HTMLFormElement>) {
     event.preventDefault();
+    setErro(null);
+    if(!valor || Number(valor)  <= 0){
+      setErro("Informe um valor válido");
+      return;
+    }
+    try{
+        const dados = await converterMoeda(
+          moedaOrigem,
+          moedaDestino,
+          Number(valor),
+        );
 
-    const dados = await converterMoeda(
-      moedaOrigem,
-      moedaDestino,
-      Number(valor),
-    );
-
-    setResultado(dados.conversion_result);
-    setTaxa(dados.conversion_rate);
+        setResultado(dados.conversion_result);
+        setTaxa(dados.conversion_rate);
+      }catch{
+        setErro("Erro ao converter. Tente novamente.");
+      }
   }
-
   return (
     <section className="currencyConverter">
       <div className="titulo">
@@ -43,15 +60,13 @@ export default function CurrencyConverter() {
           <div className="input-group">
             <label htmlFor="moedaOrigem">Converter de</label>
 
-            <select
+            <input
               id="moedaOrigem"
-              value={moedaOrigem}
-              onChange={(evento) => setMoedaOrigem(evento.target.value)}
-            >
-              <option value="USD">Dólar Americano</option>
-              <option value="EUR">Euro</option>
-              <option value="BRL">Real Brasileiro</option>
-            </select>
+              type="text"
+              value={busca}
+              placeholder="Digite a moeda de origem"
+              onChange={(evento) => setBusca(evento.target.value)}
+            />
           </div>
 
           <div className="input-group">
@@ -78,8 +93,7 @@ export default function CurrencyConverter() {
         <div className="linha">
           <p>
             <strong>Valor informado: </strong>
-            {valor || "0"}
-            {moedaOrigem}
+            {valor || "0"} {moedaOrigem}
           </p>
           <p>
             <strong>Valor convertido: </strong>{" "}
--- src/services/api.ts
+++ src/services/api.ts
@@ -2,13 +2,21 @@ const API_KEY = "69b372c596fe385410f672af";
 const BASE_URL = `https://v6.exchangerate-api.com/v6/${API_KEY}`;
 
 export async function converterMoeda(
-  moedaOrigem: String,
-  moedaDestino: String,
-  valor: Number,
-) {
+  moedaOrigem: string,
+  moedaDestino: string,
+  valor: number,
+)
+
+{
   const resposta = await fetch(
     `${BASE_URL}/pair/${moedaOrigem}/${moedaDestino}/${valor}`,
   );
+  if (!resposta.ok) throw new Error("Erro ao buscar conversão, tente novamente mais tarde");
   const dados = await resposta.json();
   return dados;
+}
+export async function listarMoedas() {
+  const lista = await fetch(`${BASE_URL}/codes/`);
+  const dadosLista = await lista.json();
+  return dadosLista;
 }
